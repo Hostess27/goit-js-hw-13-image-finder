271d84dabb27b485cc7ed37b1581ad7c967d7148
Hostess27 <pash.pash22173@gmail.com>
2021-11-10 15:29:38 +0200

    Правки

--- src/index.js
+++ src/index.js
@@ -1,39 +1,27 @@
+import './styles.css'
 import fetchPhoto from './js/apiService';
-import showErrorNotify from './js/show-error'
-import  galleryItems from './templates/photo-result.hbs';
+import showErrorNotify from './js/show-error';
+import gallery from './templates/photo-result.hbs';
 
 
 
-const gallery = document.querySelector('.gallery');
-const searchForm = document.querySelector('.search-form');
-const morePhoto =document.querySelector('.more-photo');
+// const gallery = document.querySelector('.gallery');
+// const searchForm = document.querySelector('.search-form');
 
 
+// searchForm.addEventListener('submit', onSearch);
+// function onSearch(e) {
+//   e.preventDefault();
+//   fetchPhoto.query = e.currentTarget.elements.query.value;
 
-searchForm.addEventListener('submit', onSearch)
-function onSearch(e) {
-    e.preventDefault();
-    fetchPhoto.query = e.currentTarget.elements.query.value;
+//   if (fetchPhoto.query === '') {
+//     return showErrorNotify();
+//   }
 
-    if (fetchPhoto.query ==='') {
-       return showErrorNotify();
-    } 
-
-    gallery.insertAdjacentHTML('beforeend', galleryItems)
-}
-
-// function addGallery(){
-//     gallery.insertAdjacentHTML('beforeend', galleryItems)
+//   addGallery();
+// }
+// function addGallery() {
+//   gallery.insertAdjacentHTML('beforeend', galleryItems);
 // }
 
-
-// morePhoto.addEventListener('submit');
-
-
-  
-
-
-
-
-
-
+// // LoadMoreBtn.addEventListener('submit');
